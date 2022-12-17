26564745b7dfaae7b30a011ca97cc0476bc8a9e9
Tony Rochman <alrochman23@gmail.com>
2022-12-16 21:06:27 -0800

    trying to get vercel to work still

--- week14-15-app/pages/_app.js
+++ week14-15-app/pages/_app.js
@@ -2,16 +2,17 @@ import Head from 'next/head'
 import '../styles/bootstrap.min.css'
 import '../styles/globals.css'
 import Header from '../components/Header';
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ChakraProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
       <Header/>
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   );
 }
 

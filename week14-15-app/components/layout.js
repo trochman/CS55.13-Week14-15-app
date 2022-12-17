ff7bfbe51e33c66160e32fd3cced7b435e35e5c8
Tony Rochman <alrochman23@gmail.com>
2022-12-16 17:09:28 -0800

    editing chakra ui

--- week14-15-app/components/layout.js
+++ week14-15-app/components/layout.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from 'next/head';
-import Link from 'next/link';
+import Header from '../components/Header';
 
 export default function Layout( { children, home } ) {
   return (
@@ -9,12 +9,7 @@ export default function Layout( { children, home } ) {
         <title>Tony Rochmans Basic Next.js App</title>
       </Head>
       <header>
-        <nav>
-          <Link href="/">Home Page</Link> &nbsp;|&nbsp;
-          <Link href={'/product/product_list'}>Products</Link> &nbsp;|&nbsp;
-          <Link href={'/contact/contact_list'}>Contacts</Link> &nbsp;|&nbsp;
-          <Link href={'/manufacturer/manufacturer_list'}>Manufacturers</Link>
-        </nav>
+        <Header/>
       </header>
       <main>{children}</main>
     </ChakraProvider>
